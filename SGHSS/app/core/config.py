85c695b2c0059ce65ab65ab29ad6ad81511881b7
Yara-Godoy <133469967+Scarlet-Code@users.noreply.github.com>
2025-12-18 10:27:31 -0300

    Add files via upload

---
+++ SGHSS/app/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DB_HOST: str = "localhost"  # <-- TROCAR AQUI
+    DB_PORT: str = "5432"
+    DB_NAME: str = "sghss"
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "mudar123"
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
---
+++ SGHSS/app/schemas/paciente_schema.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+
+class PacienteBase(BaseModel):
+    nome: str
+    data_nascimento: date
+    sexo: str
+    cpf: str
+    telefone: Optional[str] = None
+    endereco: Optional[str] = None
+
+class PacienteCreate(PacienteBase):
+    pass
+
+class PacienteOut(PacienteBase):
+    id_paciente: int
+
+    class Config:
+        orm_mode = True
